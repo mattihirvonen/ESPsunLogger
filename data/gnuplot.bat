d6bc1cc30d69c7508a725fd8b999fba7ab43f4a9
mattihirvonen <kuukkeli.matti.hirvonen@gmail.com>
2026-07-28 16:47:37 +0300

    Add small gnuplot script example

---
+++ data/gnuplot.bat
@@ -0,0 +1,25 @@
+@echo off
+
+set  DATA2GP=data2gp.exe
+set  GNUPLOT=c:\pgm\gnuplot\bin\gnuplot.exe
+
+rem  ================================================
+
+rem  echo open 192.168.2.1   >ftpscript.txt
+rem  echo root              >>ftpscript.txt
+rem  echo rootpassword      >>ftpscript.txt
+rem  echo cd  /             >>ftpscript.txt
+rem  echo lcd .             >>ftpscript.txt
+rem  echo get history.dat   >>ftpscript.txt
+rem  echo bye               >>ftpscript.txt
+
+rem  ftp  -s:ftpscript.txt
+
+rem  start 20:15
+rem  %DATA2GP%  -f history.dat.alku >history.txt
+rem  %DATA2GP%  -f history.dat      >history.txt
+
+rem ================================================
+
+copy          %1%                history.txt
+%GNUPLOT%  -p gnuplot_script.gp  history.txt
