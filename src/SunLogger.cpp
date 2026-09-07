4e3ee7034325feb6d8d63da38c067389e79cca32
mattihirvonen <kuukkeli.matti.hirvonen@gmail.com>
2026-09-07 09:47:38 +0300

    Refactoring and add telnetCommandHandlerCallback() example

--- src/SunLogger.cpp
+++ src/SunLogger.cpp
@@ -27,49 +27,18 @@
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
+// Choose your server's name - this is how Telnet server would introduce itself to the clients
+#define  HOSTNAME  "esp32server"
+
 #include <LittleFS.h>             // Or FFat.h or/and SD.h
 #include <threadSafeFS.h>         // Include thread-safe wrapper since LittleFS, FFat and SD file systems are not thread safe
-using File = threadSafeFS::File;  // Use thread-safe wrapper for all file operations form now on in your code
-
-#include <ntpClient.h>            // NTP client is neede only for time commands
-#define HOSTNAME "esp32server"    // Choose your server's name - this is how Telnet server would introduce itself to the clients
-
-// 1️⃣ Choose which built-in Telnet commands will be included
-#define TELNET_CLEAR_COMMAND    1   // 0=exclude, 1=include, clear is included by default
-#define TELNET_UNAME_COMMAND    1   // 0=exclude, 1=include, uname is included by default
-#define TELNET_FREE_COMMAND     1   // 0=exclude, 1=include, free is included by default
-#define TELNET_NOHUP_COMMAND    1   // 0=exclude, 1=include, nohup is included by default
-#define TELNET_REBOOT_COMMAND   1   // 0=exclude, 1=include, reboot is included by default
-#define TELNET_DMESG_COMMAND    1   // 0=exclude, 1=include, dmesg is included by default
-#define TELNET_QUIT_COMMAND     1   // 0=exclude, 1=include, quit is included by default
-#define TELNET_UPTIME_COMMAND   1   // 0=exclude, 1=include, date is included by default
-#define TELNET_DATE_COMMAND     1   // 0=exclude, 1=include, date is included by default
-#define TELNET_NTPDATE_COMMAND  1   // 0=exclude, 1=include, ntpdate is included by default
-#define TELNET_PING_COMMAND     1   // 0=exclude, 1=include, ping is included by default
-#define TELNET_IFCONFIG_COMMAND 1   // 0=exclude, 1=include, ifconfig is included by default
-#define TELNET_IW_COMMAND       1   // 0=exclude, 1=include, iw is included by default
-#define TELNET_NETSTAT_COMMAND  1   // 0=exclude, 1=include, netstat is included by default
-#define TELNET_KILL_COMMAND     1   // 0=exclude, 1=include, kill is included by default
-#define TELNET_CURL_COMMAND     1   // 0=exclude, 1=include, curl is included by default
-#define TELNET_SENDMAIL_COMMAND 1   // 0=exclude, 1=include, sendmail is included by default
-#define TELNET_LS_COMMAND       1   // 0=exclude, 1=include, ls is included by default
-#define TELNET_TREE_COMMAND     1   // 0=exclude, 1=include, tree is included by default
-#define TELNET_MKDIR_COMMAND    1   // 0=exclude, 1=include, mkdir is included by default
-#define TELNET_RMDIR_COMMAND    1   // 0=exclude, 1=include, rmdir is included by default
-#define TELNET_CD_COMMAND       1   // 0=exclude, 1=include, cd is included by default
-#define TELNET_PWD_COMMAND      1   // 0=exclude, 1=include, pwd is included by default
-#define TELNET_CAT_COMMAND      1   // 0=exclude, 1=include, cat is included by default
-#define TELNET_VI_COMMAND       1   // 0=exclude, 1=include, vi is included by default
-#define TELNET_CP_COMMAND       1   // 0=exclude, 1=include, cp is included by default
-#define TELNET_RM_COMMAND       1   // 0=exclude, 1=include, rm is included by default
-#define TELNET_LSOF_COMMAND     1   // 0=exclude, 1=include, lsof is included by default
-
-#define SWAP_DEL_AND_BACKSPACE  0   // seto to 1 to swap the meaning of these keys - this would be suitable for Putty and Linux Telnet clients
-
+#include <ntpClient.h>            // NTP client is needed only for time commands
+#include "telnetConfig.h"         // Local config enable/disable telnet server's built in command set
 #include <telnetServer.h>
 #include <ftpServer.h>
 
 // Crete thread-safe wrapper arround LittleFS (or FFat or SD)
+using  File = threadSafeFS::File; // Use thread-safe wrapper for all file operations form now on in your code
 threadSafeFS::FS TSFS (LittleFS);
 
 telnetServer_t *telnetServer = NULL;
@@ -81,7 +50,7 @@ ftpServer_t    *ftpServer    = NULL;
 
 #define WIFI_ACCESSPOINT  0     // Zero: connect to WiFi router
 #define MQTT_SERVER       0     // Local MQTT broker (future feature)
-#define MQTT_CLIENT       0     // Connect to MQTT broker?
+#define MQTT_CLIENT       1     // Connect to MQTT broker?
 
 //
 // Calibration info:
@@ -153,33 +122,36 @@ void messageReceived(String &topic, String &payload) {
 }
 
 
-void connect_MQTT() {
-  #if WIFI_ACCESSPOINT == 0
-  Serial.print("\nChecking   WiFi...");
-  while (WiFi.status() != WL_CONNECTED) {
-    digitalWrite(LED, LOW);  // Turn the LED off
-    Serial.print(".");
-    delay(1000);
+void connect( int wifi_accesspoint, int mqtt_client )
+{
+  if ( ! wifi_accesspoint )
+  {
+    Serial.print("\nChecking   WiFi...");
+    while (WiFi.status() != WL_CONNECTED) {
+      digitalWrite(LED, LOW);  // Turn the LED off
+      Serial.print(".");
+      delay(1000);
+    }
+    Serial.print("\nConnected  WiFi");
   }
-  Serial.print("\nConnected  WiFi");
-  #endif // WIFI_ACCESSPOINT
 
-  #if MQTT_CLIENT
-  Serial.print("\nConnecting MQTT...");
-//while (!mqttClient.connect(MQTT_CLIENT_ID)) {
-//while (!mqttClient.connect(MQTT_CLIENT_ID, MQTT_USERNAME, MQTT_PASSWORD)) {   // "public.cloud.shiftr.io"
-  while (!mqttClient.connect(MQTT_CLIENT_ID, "", "")) {                         // "test.mosquitto.org"
-    Serial.print(".");
-    delay(1000);
+  if ( mqtt_client )
+  {
+    Serial.print("\nConnecting MQTT...");
+  //while (!mqttClient.connect(MQTT_CLIENT_ID)) {
+  //while (!mqttClient.connect(MQTT_CLIENT_ID, MQTT_USERNAME, MQTT_PASSWORD)) {   // "public.cloud.shiftr.io"
+    while (!mqttClient.connect(MQTT_CLIENT_ID, "", "")) {                         // "test.mosquitto.org"
+      Serial.print(".");
+      delay(1000);
+    }
+    Serial.println("\nConnected  MQTT");
+
+    #if MQTT_SUBSCRIBE
+    mqttClient.subscribe(MQTT_TOPIC);
+  //mqttClient.unsubscribe(MQTT_TOPIC);
+    #endif // MQTT_SUBSCRIBE
   }
-  Serial.println("\nConnected  MQTT");
   digitalWrite(LED, HIGH);  // Turn the LED on
-
-  #if MQTT_SUBSCRIBE
-  mqttClient.subscribe(MQTT_TOPIC);
-//client.unsubscribe(MQTT_TOPIC);
-  #endif // MQTT_SUBSCRIBE
-  #endif // MQTT_CLIENT
 }
 
 //-----------------------------------------------------------------------------------------
@@ -271,6 +243,53 @@ void taskMeasure( void UNUSED *pvParameters )
 }
 
 
+//--------------------------------------------------------------------------------------------
+
+// Provide callback function that would handle user-defined commands
+String telnetCommandHandlerCallback (int argc, char *argv [], telnetServer_t::telnetConnection_t *tcn)
+{
+    #ifndef LED_BUILTIN
+    #define LED_BUILTIN  2
+    #endif
+
+    // Must be reentrant !!!
+
+    #define argv0is(X) (argc > 0 && !strcmp (argv[0], X))  
+    #define argv1is(X) (argc > 1 && !strcmp (argv[1], X))
+    #define argv2is(X) (argc > 2 && !strcmp (argv[2], X))   
+
+    // Short-running functions should return the text the Telnet server will send to the client as a response to the command
+    if (argv0is ("turn") && argv1is ("led") && argv2is ("on")) {
+            digitalWrite (LED_BUILTIN, HIGH);
+            return "Led is on";
+    } else if (argv0is ("turn") && argv1is ("led") && argv2is ("off")) {
+            digitalWrite (LED_BUILTIN, LOW);
+            return "Led is off";
+    }
+
+    // Long-running functions should provide a mechanism to break the loop 
+    else if (argv0is ("led") && argv1is ("state")) {
+            for (int i = 0; i < 1000; i++) {
+                char buf [6];
+                sprintf (buf, "%s", digitalRead (LED_BUILTIN) ? "on\r\n" : "off\r\n");
+                if (tcn->sendString (buf) <= 0)
+                  return "\r";
+                delay (250); 
+
+                if (tcn->peekChar ()) {
+                    tcn->recvChar ();
+                    return "\r"; // break the loop and return something different than "" to let the telnet server function know that the command has been processed
+                }
+            }
+            return "\r"; // return something different than "" to let the telnet server function know that the command has been processed
+    }
+
+    // Unhandeled - let the Telnet server try to handle the command itself
+    return "";
+}
+
+//--------------------------------------------------------------------------------------------
+
 void setup( void )
 {
     Serial.begin( 115200 );
@@ -290,6 +309,7 @@ void setup( void )
     #else
     // Connect to Wi-Fi router
     setup_wifi( ssid, password );
+    digitalWrite(LED, HIGH);        // Turn the LED on
     #endif // WIFI_ACCESSPOINT
 
     #if MQTT_CLIENT
@@ -299,16 +319,17 @@ void setup( void )
     mqttClient.begin(MQTT_BROKER, wifiClient);
     mqttClient.onMessage(messageReceived);
 
-    connect_MQTT();
+    connect(  WIFI_ACCESSPOINT, MQTT_CLIENT );
     #endif // MQTT_CLIENT
 
     // Create Telnet server instance that would use thread-safe wrapper arround LittleFS (or FFat or SD)
-    telnetServer = new (std::nothrow) telnetServer_t (TSFS);  // optional arguments:
-                                                              // Cstring<255> (*getUserHomeDirectory) (const Cstring<64>& userName, const Cstring<64>& password) = NULL
-                                                              // String (*telnetCommandHandlerCallback) (int argc, char *argv [], telnetConnection_t *tcn) = NULL
-                                                              // int serverPort = 23
-                                                              // bool (*firewallCallback) (char *clientIP, char *serverIP) = NULL
-                                                              // bool runListenerInItsOwnTask = true
+    telnetServer = new (std::nothrow) telnetServer_t (TSFS, // optional arguments:
+                                                      NULL, telnetCommandHandlerCallback, 23, NULL, true);
+                                                      // Cstring<255> (*getUserHomeDirectory) (const Cstring<64>& userName, const Cstring<64>& password) = NULL
+                                                      // String (*telnetCommandHandlerCallback) (int argc, char *argv [], telnetConnection_t *tcn) = NULL
+                                                      // int serverPort = 23
+                                                      // bool (*firewallCallback) (char *clientIP, char *serverIP) = NULL
+                                                      // bool runListenerInItsOwnTask = true
 
     // Check if Telnet server instance is created && Telnet server is running
     if (telnetServer && *telnetServer)  Serial.println ("Telnet server started");
@@ -356,7 +377,6 @@ void loop( void )
     int32_t  now = millis();
 
     // - - - - - - - - - - - - - - - - - - - - - - -
-
     #if WIFI_ACCESSPOINT
 
     #define BLINK   1000L //   [ms]
@@ -370,9 +390,7 @@ void loop( void )
     }
 
     #endif // WIFI_ACCESSPOINT
-
     // - - - - - - - - - - - - - - - - - - - - - - -
-
     #if MQTT_CLIENT
     
     #define PERIOD  1000L  // [ms]
@@ -386,7 +404,7 @@ void loop( void )
     delay(10);         // <- fixes some issues with WiFi stability
 
     if ( !mqttClient.connected() ) {
-        connect();
+        connect( WIFI_ACCESSPOINT, MQTT_CLIENT );
     }
 
     // Publish MQTT message every 1 seconds (PERIOD)
@@ -424,4 +442,5 @@ void loop( void )
     Serial.print(line);
 
     #endif // MQTT_CLIENT
+    // - - - - - - - - - - - - - - - - - - - - - - -
 }
